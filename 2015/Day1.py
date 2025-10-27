4d97ae7b59448448c7fba6ef4b2350b5bbe47269
Dorayakis007 <136026620+Dorayakis007@users.noreply.github.com>
2025-10-27 05:30:41 +0100

    Added Day 1 and Day 4, also added 2015 folder

---
+++ 2015/Day1.py
@@ -0,0 +1,41 @@
+the_input_string:str = ""
+the_input_list:list = [""]
+positive_parenthesis:int = 0
+negative_parenthesis:int = 0
+total_floors:int = 0
+current_list_spot:int = 0
+current_list_value:str = ""
+
+the_input_string = input("Input the Input: ")
+the_input_list = list(the_input_string)
+
+
+# Part 1
+positive_parenthesis = the_input_list.count("(")
+negative_parenthesis = the_input_list.count(")")
+
+total_floors = positive_parenthesis - negative_parenthesis
+
+print(total_floors)
+
+
+#Limpiando variables
+positive_parenthesis = 0
+negative_parenthesis = 0
+
+
+#Part 2
+while True:
+	current_list_value = the_input_list[current_list_spot]
+
+	match current_list_value:
+		case "(":
+			positive_parenthesis += 1
+		case ")":
+			negative_parenthesis += 1
+	
+	if(positive_parenthesis - negative_parenthesis == -1):
+		print(current_list_spot + 1)
+		break
+	else:
+		current_list_spot += 1
---
+++ 2015/Day4.py
@@ -0,0 +1,38 @@
+import hashlib
+md5_hash:str = ""
+user_input:str = ""
+hash_digerido:str = ""
+cantidad_ceros:int = 0
+conseguido:bool = False
+input_con_num:str = ""
+numero_actual:int = -1
+ceros_queridos:int = 0
+
+user_input = input("Input: ") # Pilla el user input
+ceros_queridos = int(input("Por cuantos ceros quieres que empiece el hash? "))
+
+while not(conseguido):
+	numero_actual += 1 
+	input_con_num = str(user_input) + str(numero_actual) # Junta input con el numero
+	md5_hash = hashlib.md5() # Reiniciamos el hash
+	md5_hash.update(input_con_num.encode()) # Actualiza el md5
+	hash_digerido = md5_hash.hexdigest() # Actualiza la variable con el hex del hash
+
+	# Checkeo de si da cinco ceros o no
+	for char in list(hash_digerido):
+		if (char == "0"):
+			cantidad_ceros += 1
+			if (cantidad_ceros == ceros_queridos):
+				conseguido = True
+				break
+			else:
+				continue
+		else:
+			break
+	cantidad_ceros = 0
+
+print("")
+print("Hash final:", hash_digerido)
+print("Input final:", input_con_num)
+print("Numero solo:", numero_actual)
+print("Lo hice?: ", conseguido)
